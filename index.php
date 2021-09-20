289e3bd48b054acb1840df61941a7764f02f1284
nvasev32 <91050777+nvasev32@users.noreply.github.com>
2021-09-20 10:24:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
-echo "Hello World!";
+echo "Hello World! 00:1a:79:00:1c:df, October10, 2023,http://mag.cdn-ky.com/
+00:1a:79:02:06:bd, May22, 2025,http://mag.cdn-ky.com/
+00:1a:79:02:0a:a8, July28, 2022,http://mag.cdn-ky.com/";
